b7374c80f8e620c976463495d961834685ef888a
Sneha SK <somalingasneha4@gmail.com>
2025-03-10 17:51:50 +0530

    commit

--- src/config/Gemini.js
+++ src/config/Gemini.js
@@ -9,7 +9,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const MODEL_NAME = "gemini-1.0-pro";
+const MODEL_NAME = "gemini-1.5-flash";
 const API_KEY = "Your Api Key ";
 
 async function runChat(prompt) {
